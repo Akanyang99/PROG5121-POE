62d7710e6be44153ddb34d36cc17b48c4466f021
Akanyang99 <akanyangc1@gmail.com>
2026-04-20 13:52:01 +0200

    Update RegistrationTest.java
    code fixed

--- RegistrationTest.java
+++ RegistrationTest.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.registration;
 
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,12 +42,28 @@ public class RegistrationTest {
      * Test of main method, of class Registration.
      */
     @Test
+public void testRegister() {
+    String simulatedInput = "newUser\nMyPass123\n1234567890\n";
+    Scanner sc = new Scanner(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+    Login login = new Login();
+    login.register(sc);
+
+    assertTrue(login.checkUserName("newUser"));  // verify registration
+}
+
+    /**
+     * Test of main method, of class Registration.
+     */
+    @org.junit.Test
     public void testMain() {
         System.out.println("main");
         String[] args = null;
         Registration.main(args);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
     
 }
+
+
